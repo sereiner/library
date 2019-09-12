3e215eb68afcfa526941605cf3124d68fed8541b
wule61 <wule61@live.com>
2019-09-12 11:52:44 +0800

    fix retry

--- utility/retry.go
+++ utility/retry.go
@@ -1,27 +1,21 @@
 package utility
 
-import (
-	"errors"
-	"log"
-	"time"
-)
+//var CanStopErr = errors.New("retry can stop")
 
-var CanStopErr = errors.New("retry can stop")
-
-func Retry(attempts int, sleep time.Duration, fn func() error) error {
-
-	if err := fn(); err != nil {
-		if errors.Is(err, CanStopErr) {
-			return err
-		}
-
-		if attempts--; attempts > 0 {
-			log.Printf("retry func error: %s. attemps #%d after %s.\n", err.Error(), attempts, sleep)
-			time.Sleep(sleep)
-			return Retry(attempts, 2*sleep, fn)
-		}
-		return err
-	}
-
-	return nil
-}
+//func Retry(attempts int, sleep time.Duration, fn func() error) error {
+//
+//	if err := fn(); err != nil {
+//		if errors.Is(err, CanStopErr) {
+//			return err
+//		}
+//
+//		if attempts--; attempts > 0 {
+//			log.Printf("retry func error: %s. attemps #%d after %s.\n", err.Error(), attempts, sleep)
+//			time.Sleep(sleep)
+//			return Retry(attempts, 2*sleep, fn)
+//		}
+//		return err
+//	}
+//
+//	return nil
+//}
